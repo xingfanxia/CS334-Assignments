ddf54b82da9ac1e7bd381a1022bdb30a9b1bfbdb
xxf1995 <xiax@carleton.edu>
2016-09-21 10:56:01 -0500

    little tweak

--- ps03/queries.sql
+++ ps03/queries.sql
@@ -1,7 +1,7 @@
 -- 1. Print the names of professors who work in departments that have fewer than 50 PhD students.
 select pname
 from prof as P, dept as D
-where p.dname = D.dname 
+where P.dname = D.dname 
 	and numphds<50;
 
 -- 2. Print the names of the students with the lowest GPA.
